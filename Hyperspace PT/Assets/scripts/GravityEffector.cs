05b99d594dc4662ed3ae7558a4c93bdf57e14d5f
steeb-dev <steeb.dev@gmail.com>
2016-05-18 15:47:37 +1000

    steeb - added a gravity manager system as a test

---
+++ Hyperspace PT/Assets/scripts/GravityEffector.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GravityEffector : MonoBehaviour {
+
+    public float size;
+    public float mass;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public Vector3 GetForceAtPosition(Vector3 objectPosition, float objectMass)
+    {
+        //calculate force using the size mass relative to the object position
+        float summedMass = mass + objectMass;
+
+        float gravitational_constant = 100 ;
+        float distance_squared = (this.transform.position - objectPosition).sqrMagnitude;
+
+        float force = gravitational_constant * ((this.mass * objectMass) / distance_squared);
+  
+        // apply the force from the player toward the planet
+        Vector3 force_direction = (this.transform.position - objectPosition).normalized;
+        Vector3 force_vector = force_direction * force;
+
+        return force_vector;
+    }   
+
+}
---
+++ Hyperspace PT/Assets/scripts/GravityManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class GravityManager : MonoBehaviour
+{
+    public List<GravityEffector> m_ActiveGravityEffectors = new List<GravityEffector>();
+    
+    //Refresh collection of effectors
+    public void UpdateGravityEffectors()
+    {
+        m_ActiveGravityEffectors.Clear();
+        GravityEffector[] activeEffectors = FindObjectsOfType<GravityEffector>();
+        foreach (GravityEffector ge in activeEffectors)
+        {
+
+            m_ActiveGravityEffectors.Add(ge);
+        }
+    }
+
+    public Vector3 GetSummedGravityForceAtPosition(Vector3 objectPosition, float mass)
+    {
+        Vector3 finalForce = objectPosition;
+        foreach (GravityEffector ge in m_ActiveGravityEffectors)
+        {
+            Vector3 addForce = ge.GetForceAtPosition(objectPosition, mass);
+            finalForce += addForce;
+        }
+        return finalForce;
+    }
+}
